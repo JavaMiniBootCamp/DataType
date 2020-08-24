1336c969067185b7fa5cbebacb15f670aa6b4d1b
SienTien <hsientien0212@gmail.com>
2020-08-24 16:01:32 -0400

    first commit

---
+++ src/DataTypes.java
@@ -0,0 +1,11 @@
+public class DataTypes {
+ public static void main(String[] args) {
+     int number = 233;
+     boolean isRaining;
+     boolean isSunny = false;
+     byte b = 1;
+     char single = 's';
+     System.out.println("the number is "+ (number + 1));
+
+ }
+}
